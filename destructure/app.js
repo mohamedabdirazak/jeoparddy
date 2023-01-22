e06b546f1977f8c849f67464c2a45cdda3fb7ff0
mohamedabdirazak <qaydiski@gmail.com>
2023-01-21 18:10:32 -0600

    exercise

---
+++ destructure/app.js
@@ -0,0 +1,44 @@
+// Object Destructuring 1
+// What does the following code return/print?
+
+let facts = {numPlanets: 8, yearNeptuneDiscovered: 1846};
+let {numPlanets, yearNeptuneDiscovered} = facts;
+
+console.log(numPlanets); // 8
+console.log(yearNeptuneDiscovered); // 1846
+
+// What does the following code return/print?
+
+let planetFacts = {
+  numPlanets: 8,
+  yearNeptuneDiscovered: 1846,
+  yearMarsDiscovered: 1659
+};
+
+function getUserData({firstName, favoriteColor="green"}){
+  return `Your name is ${firstName} and you like ${favoriteColor}`;
+}
+
+getUserData({firstName: "Alejandro", favoriteColor: "purple"})
+
+
+getUserData({firstName: "Melissa"})
+
+
+getUserData({})
+
+
+let [first, second, third] = ["Maya", "Marisa", "Chi"];
+
+console.log(first); 
+console.log(second); 
+console.log(third); 
+
+
+
+//What does the following code return/print?
+
+let numbers = [10, 20, 30];
+[numbers[1], numbers[2]] = [numbers[2], numbers[1]]
+
+console.log(numbers) // [10,30,20]
